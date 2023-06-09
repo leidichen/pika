ef7750d1c221be6cd0acd33be8333e5c32e01e1d
leidichen <leidison@163.com>
2023-06-09 14:29:44 +0800

    更新14:29

--- components/main.js
+++ components/main.js
@@ -458,7 +458,7 @@ export default function Main() {
                                     "bg-gradient-to-br from-red-300 to-purple-600",
                                     "bg-white",
                                     "bg-black",
-                                    "bg-gradient-to-br from-red-100 to-purple-400",
+                                    "bg-gradient-to-br from-#030e26 to-#030e26",
                                 ].map((theme) => (
                                     <div
                                         key={theme}
